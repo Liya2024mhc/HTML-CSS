b5e357db5d3f972208af6214dafc9a07f50ddbc0
Liya2024mhc <liya@myheartcreative.com>
2024-11-01 14:12:35 +0530

    Create next.js

---
+++ next.js
@@ -0,0 +1,7 @@
+https://www.educative.io/blog/nextjs-tutorial-examples
+https://www.freecodecamp.org/news/build-a-full-stack-application-with-nextjs/
+https://nextjs.org/learn-pages-router/basics/create-nextjs-app
+https://github.com/vercel/next.js/tree/canary/examples/with-emotion
+https://github.com/vercel/next.js/tree/canary/examples/with-styled-components
+https://github.com/vercel/styled-jsx
+https://github.com/lukeed/clsx
